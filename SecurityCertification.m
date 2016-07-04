a67ac03d7653a889ba98a90075e446ed15d7b5c1
Yan <myncepu@gmail.com>
2016-07-04 10:12:04 +0800

    SecurityCertification script->function

--- SecurityCertification.m
+++ SecurityCertification.m
@@ -1,4 +1,5 @@
-% function [flagVoltage, voltage, flagPower, Power] = SecurityCertification(filename, iBranch)
+function [branchOff, flagVoltageViolation, voltageViolation, flagPowerViolation, powerViolation] = ...
+    SecurityCertification(filename, iBranch)
 
 [PQ, PV, REF, NONE, BUS_I, BUS_TYPE, PD, QD, GS, BS, BUS_AREA, VM, ...
     VA, BASE_KV, ZONE, VMAX, VMIN, LAM_P, LAM_Q, MU_VMAX, MU_VMIN] = idx_bus;
@@ -11,54 +12,37 @@
 % IEEE14 包含20个支路，增加一个支路
 % n-1 安全校验, 依次断开每条线路，计算潮流
 mpopt = mpoption('verbose', 0, 'out.all', 0); % 潮流计算不输出结果
-r0 = runpf('my_case14.m', mpopt);
-for iBranch = 1:21
-    MPC = loadcase('my_case14.m');
-    MPC.branch(iBranch, :) = [];
-    savecase('my_case14_SC.m', MPC);
-    r(iBranch) = runpf('my_case14_SC.m', mpopt);
-    flagVoltage(iBranch) = any(abs(r(iBranch).bus(:, VM) - 1) > 0.5);
-    branchPowerOrigin = r0.branch(:, [PF, QF, PT, QT]);
-    branchPowerOrigin(iBranch, :) = [];
-    branchApparentPowerOrigin = [sqrt(branchPowerOrigin(:, 1) .^ 2 + branchPowerOrigin(:, 2) .^ 2) ...
-        sqrt(branchPowerOrigin(:, 3) .^ 2 + branchPowerOrigin(:, 4) .^ 2)];
-    branchPowerNow = r(iBranch).branch(:, [PF, QF, PT, QT]);
-    branchApparentPowerNow = [sqrt(branchPowerNow(:, 1) .^ 2 + branchPowerNow(:, 2) .^ 2) ...
-        sqrt(branchPowerNow(:, 3) .^ 2 + branchPowerNow(:, 4) .^ 2)];
-    powerViolationFlag = branchApparentPowerNow > 1.5 * branchApparentPowerOrigin;
-    powerViolation(:, :, iBranch) = powerViolationFlag;
-    powerViolationBranch = [r(iBranch).branch(:, [F_BUS, T_BUS]), powerViolationFlag];
-    temp = [];
-    for j = 1:20
-        if any(powerViolationBranch(j, 3:4) ~= 0)
-            temp = [temp; powerViolationBranch(j, :)];
-        end
-    end
-    flagPowerViolationBranch{iBranch} = temp;
-    flagPower(:, :, iBranch) = any(any(branchApparentPowerNow > 1.5 * branchApparentPowerOrigin));
-end
+rOrigin = runpf('my_case14.m', mpopt); % 原来电网潮流计算
 
-% %% 1-2 支路断
-% MPC = loadcase('my_case14.m');
-% MPC.branch(1, :) = [];
-% % MPC.gen(1, [PG, QG]) = MPC.gen(1, [PG, QG]) / 2;
-% MPC.bus(:, [PD, QD]) = MPC.bus(:, [PD, QD]) / 2;
-% savecase('my_case14_SC.m', MPC);
-% result = runpf('my_case14_SC.m');
-% 
-% branchPowerOrigin = r0.branch(:, [PF, QF, PT, QT]);
-% branchPowerOrigin(1, :) = [];
-% branchApparentPowerOrigin = [sqrt(branchPowerOrigin(:, 1) .^ 2 + branchPowerOrigin(:, 2) .^ 2) ...
-%     sqrt(branchPowerOrigin(:, 3) .^ 2 + branchPowerOrigin(:, 4) .^ 2)];
-% branchPowerNow = result.branch(:, [PF, QF, PT, QT]);
-% branchApparentPowerNow = [sqrt(branchPowerNow(:, 1) .^ 2 + branchPowerNow(:, 2) .^ 2) ...
-%     sqrt(branchPowerNow(:, 3) .^ 2 + branchPowerNow(:, 4) .^ 2)];
-% powerViolationFlag = branchApparentPowerNow > 1.5 * branchApparentPowerOrigin;
-% powerViolationBranch = [result.branch(:, [F_BUS, T_BUS]), powerViolationFlag];
-% temp = [];
-% for j = 1:20
-%     if any(powerViolationBranch(j, 3:4) ~= 0)
-%         temp = [temp; powerViolationBranch(j, :)];
-%     end
-% end
-% flagPowerViolationBranch = temp;
+MPC = loadcase(filename);
+branchOff = MPC.branch(iBranch, [F_BUS, T_BUS]);
+MPC.branch(iBranch, :) = []; % 断开支路iBranch
+
+savecase('my_case14_SC.m', MPC);
+rNow = runpf('my_case14_SC.m', mpopt); % 新电网潮流计算
+flagVoltageViolation = any(abs(rNow.bus(:, VM) - 1) > 0.5); % 判断断开支路后电网电压是否越限
+voltageViolation = [];
+for k = 1:size(MPC.bus)
+    if abs(rNow.bus(k, VM) - 1) > 0.5
+        voltageViolation = [voltageViolation; rNow.bus(k, [BUS_I, VM])];
+    end
+end
+branchPowerOrigin = rOrigin.branch(:, [PF, QF, PT, QT]);
+branchPowerOrigin(iBranch, :) = [];
+branchApparentPowerOrigin = [sqrt(branchPowerOrigin(:, 1) .^ 2 + branchPowerOrigin(:, 2) .^ 2) ...
+    sqrt(branchPowerOrigin(:, 3) .^ 2 + branchPowerOrigin(:, 4) .^ 2)];
+branchPowerNow = rNow.branch(:, [PF, QF, PT, QT]);
+branchApparentPowerNow = [sqrt(branchPowerNow(:, 1) .^ 2 + branchPowerNow(:, 2) .^ 2) ...
+    sqrt(branchPowerNow(:, 3) .^ 2 + branchPowerNow(:, 4) .^ 2)];
+powerViolationFlag = branchApparentPowerNow > 1.5 * branchApparentPowerOrigin;
+flagPowerViolation = any(any(powerViolationFlag));
+powerViolationBranch = [rNow.branch(:, [F_BUS, T_BUS]), powerViolationFlag];
+powerViolation = [];
+for j = 1:size(MPC.branch, 1)
+    if any(powerViolationBranch(j, 3:4) ~= 0)
+%         powerViolation = [powerViolation; powerViolationBranch(j, :), ...
+%             branchApparentPowerOrigin(j, :), branchApparentPowerNow(j, :)];
+        powerViolation = [powerViolation; powerViolationBranch(j, 1:2), ...
+            branchApparentPowerOrigin(j, :), branchApparentPowerNow(j, :)];
+    end
+end
---
+++ main.m
@@ -0,0 +1,4 @@
+clc, clear all;
+filename = 'my_case14.m';
+iBranch = 1;
+[bOff, fV, V, fP, P] = SecurityCertification(filename, iBranch)
